7abd0d0fb1833f70bfbf9414a1693cd3201b75ca
DeShon Dixon <deshondixon@gmail.com>
2023-03-29 17:04:01 -0700

    acp

--- main.go
+++ main.go
@@ -1,8 +1,41 @@
 package main
 
-import github.com/spf13/viper
+import (
+	"context"
+	"go/scanner"
+
+	"github.com/PullRequestInc/go-gpt3"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
 
 func main() {
-	 viper.SetConfigFile(".env")
-	 viper.ReadInConfig()
+		viper.SetConfigFile(".env")
+		viper.ReadInConfig()
+		apiKey := viper.GetString("API_KEY")
+		if apiKey == "" {
+				panic("Missing API_KEY")
+		}
+
+		ctx := context.Background()
+		client := gpt3.NewClient(apiKey)
+		rootCmd := &cobra.Command{
+				Use: "chatgpt", 
+				Short: "Chat with ChatGPT in console."
+				Run: func(cmd *cobra.Command, args []string) {
+					scanner := bufio.NewScanner(os.Stdin)
+					quit := false
+
+					for  !quit{
+							 fmt.Print("Ask Genie any question and your answer shall be granted ('quit' if all questions are granted):")
+							 if !scanner.Scan() {
+								 break
+							 }
+							 question := scanner.Text()
+							 switch question {
+								case "quit":
+							 }
+					}
+		}
+	}
 }
